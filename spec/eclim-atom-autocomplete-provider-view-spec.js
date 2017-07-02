4f8e0935d42845366176e6c56854b87db3289732
kurpachsv <kurpachsv@gmail.com>
2017-07-02 21:56:10 +0300

    First autocomplete provider version

--- spec/eclim-atom-autocomplete-provider-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import EclimAtomAutocompleteProviderView from '../lib/eclim-atom-autocomplete-provider-view';
-
-describe('EclimAtomAutocompleteProviderView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
